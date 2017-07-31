6cfa4df2dae9ba0501e175be3ede4c8e2e0e82cd
jclotto <30593886+jclotto@users.noreply.github.com>
2017-07-31 13:39:39 +0200

    Update FlightDCOTest.java

--- flightsearch/src/test/java/org/lastminute/flightsearch/test/dco/FlightDCOTest.java
+++ flightsearch/src/test/java/org/lastminute/flightsearch/test/dco/FlightDCOTest.java
@@ -24,18 +24,18 @@ public class FlightDCOTest {
 		assertEquals(2,result.size());
 		assertEquals("LHR,IST,TK8891,250.0",result.get(0).getFlight().save());
 		//250*2 adults + 250*2*67% child + 2*5 infants = 845,00€
-		assertEquals("845,00",result.get(0).getTotalPrice());
+		assertEquals("845.00",result.get(0).getTotalPrice());
 		assertEquals("LHR,IST,LH1085,148.0",result.get(1).getFlight().save());
 		//148*2 adults + 148*2*67% + 2*7 = 508,32€
-		assertEquals("508,32",result.get(1).getTotalPrice());
+		assertEquals("508.32",result.get(1).getTotalPrice());
 		result = flightDCO.searchFlights("London", "Istanbul", 31, 1, 1, 1);
 		assertEquals(2,result.size());
 		assertEquals("LHR,IST,TK8891,250.0",result.get(0).getFlight().save());
 		//250*1*80% adults + 250*1*67%*80% child + 1*5 infants = 339,00€
-		assertEquals("339,00",result.get(0).getTotalPrice());
+		assertEquals("339.00",result.get(0).getTotalPrice());
 		assertEquals("LHR,IST,LH1085,148.0",result.get(1).getFlight().save());
 		//148*1*80% adults + 148*1*80%*67% + 1*7 = 204,73€
-		assertEquals("204,73",result.get(1).getTotalPrice());
+		assertEquals("204.73",result.get(1).getTotalPrice());
 		
 	}
 
